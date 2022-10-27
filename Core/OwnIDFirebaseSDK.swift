d54fcc1e280d06cb3a55752c31bf5113d95f341a
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-10-27 11:45:10 +0200

    KYIV-1217: Move Firebase To Cocoapods (#6)
    * Add podspec
    
    * Add correct pods

--- Core/OwnIDFirebaseSDK.swift
+++ Core/OwnIDFirebaseSDK.swift
@@ -1,9 +1,8 @@
-@_exported import OwnIDCoreSDK
-@_exported import OwnIDUISDK
 import FirebaseAuth
 import FirebaseFirestore
 import Combine
 import SwiftUI
+@_exported import OwnIDCoreSDK
 
 extension OwnID.FirebaseSDK {
     static let sdkName = "Firebase"
--- Core/Types/Types.swift
+++ Core/Types/Types.swift
@@ -1,7 +1,6 @@
 import OwnIDCoreSDK
 import Combine
 import FirebaseAuth
-import OwnIDFlowsSDK
 
 public extension OwnID.FirebaseSDK {
     typealias EventPublisher = AnyPublisher<VoidOperationResult, OwnID.CoreSDK.Error>
