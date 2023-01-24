fae5dfe3c9e717fcdfb60af9d669fe739af43173
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-01-24 12:23:05 +0100

    Simplify comments for functions

--- Core/OwnIDFirebaseSDK.swift
+++ Core/OwnIDFirebaseSDK.swift
@@ -23,18 +23,13 @@ public extension OwnID {
             OwnID.CoreSDK.shared.configure(userFacingSDK: info(), underlyingSDKs: [], supportedLanguages: supportedLanguages)
         }
         
-        /// Configures SDK from URL
-        /// - Parameter plistUrl: Config plist URL
+        /// Configures SDK from plist path URL
         public static func configure(plistUrl: URL, supportedLanguages: OwnID.CoreSDK.Languages = .init(rawValue: Locale.preferredLanguages)) {
             OwnID.CoreSDK.shared.configureFor(plistUrl: plistUrl, userFacingSDK: info(), underlyingSDKs: [], supportedLanguages: supportedLanguages)
         }
         
-        /// Configures SDK from parameters
-        /// - Parameters:
-        ///   - serverURL: ServerURL
-        ///   - redirectionURL: RedirectionURL
-        public static func configure(appID: String,
-                                     redirectionURL: String,
+        public static func configure(appID: OwnID.CoreSDK.AppID,
+                                     redirectionURL: OwnID.CoreSDK.RedirectionURLString,
                                      environment: String? = .none,
                                      supportedLanguages: OwnID.CoreSDK.Languages = .init(rawValue: Locale.preferredLanguages)) {
             OwnID.CoreSDK.shared.configure(appID: appID,
@@ -45,21 +40,16 @@ public extension OwnID {
                                            supportedLanguages: supportedLanguages)
         }
         
-        /// Used to handle the redirects from browser after webapp is finished
-        /// - Parameter url: URL returned from webapp after it has finished
+        /// Handles redirects from other flows back to the app
         public static func handle(url: URL) {
             OwnID.CoreSDK.shared.handle(url: url, sdkConfigurationName: sdkName)
         }
         
         // MARK: View Model Flows
         
-        /// 
-        /// - Parameters:
-        ///   - auth: Firebase Auth
-        ///   - firestore: Firestore
         public static func registrationViewModel(auth: Auth = .auth(),
                                                  firestore: Firestore = .firestore(),
-                                                 emailPublisher: AnyPublisher<String, Never>) -> OwnID.FlowsSDK.RegisterView.ViewModel {
+                                                 emailPublisher: OwnID.CoreSDK.EmailPublisher) -> OwnID.FlowsSDK.RegisterView.ViewModel {
             let performer = RegistrationPerformer(auth: auth, firestore: firestore)
             let performerLogin = LoginPerformer(auth: auth, sdkConfigurationName: sdkName)
             return OwnID.FlowsSDK.RegisterView.ViewModel(registrationPerformer: performer,
@@ -68,24 +58,19 @@ public extension OwnID {
                                                          emailPublisher: emailPublisher)
         }
         
-        ///
         public static func createRegisterView(viewModel: OwnID.FlowsSDK.RegisterView.ViewModel,
                                               visualConfig: OwnID.UISDK.VisualLookConfig = .init()) -> OwnID.FlowsSDK.RegisterView {
             OwnID.FlowsSDK.RegisterView(viewModel: viewModel, visualConfig: visualConfig)
         }
         
-        ///
-        /// - Parameters:
-        ///   - auth: Firebase Auth
         public static func loginViewModel(auth: Auth = .auth(),
-                                          emailPublisher: AnyPublisher<String, Never>) -> OwnID.FlowsSDK.LoginView.ViewModel {
+                                          emailPublisher: OwnID.CoreSDK.EmailPublisher) -> OwnID.FlowsSDK.LoginView.ViewModel {
             let performer = LoginPerformer(auth: auth, sdkConfigurationName: sdkName)
             return OwnID.FlowsSDK.LoginView.ViewModel(loginPerformer: performer,
                                                       sdkConfigurationName: sdkName,
                                                       emailPublisher: emailPublisher)
         }
         
-        ///
         public static func createLoginView(viewModel: OwnID.FlowsSDK.LoginView.ViewModel,
                                            visualConfig: OwnID.UISDK.VisualLookConfig = .init()) -> OwnID.FlowsSDK.LoginView {
             OwnID.FlowsSDK.LoginView(viewModel: viewModel, visualConfig: visualConfig)
